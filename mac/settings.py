92c521c13a86e42e54661f01e07b4a391292f9ca
MamidwarSampada <s19_mamidwar_sampada@mgmcen.ac.in>
2024-07-23 22:02:06 +0530

    Your Message

--- mac/settings.py
+++ mac/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-6&+kkdb$^lp4-4r5+*^c=+$!ch98wlzpq*f7gi)+k(_0u-)ekd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -113,7 +113,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
